f86a44d40918df14989954de78b1df67358179dd
Eyader Tsehayu <eyaderts@gmail.com>
2024-01-27 16:52:25 +0300

    initial folder structure

---
+++ app/(admin)/dashboard/layout.jsx
@@ -0,0 +1,27 @@
+"use client";
+import Navbar from "@/components/dashboard/navbar/navbar";
+import Sidebar from "@/components/dashboard/sidebar/sidebar";
+import { useState } from "react";
+import "./data-tables-css.css";
+const Layout = ({ children }) => {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  return (
+    <div>
+      <div className="flex h-screen overflow-hidden">
+        <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+
+        <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
+          {" "}
+          <Navbar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+          <main>
+            <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
+              {children}
+            </div>
+          </main>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
---
+++ components/dashboard/navbar/navbar.jsx
@@ -0,0 +1,142 @@
+import Link from "next/link";
+
+import DropdownUser from "./DropdownUser";
+import Image from "next/image";
+
+import { usePathname } from "next/navigation";
+import { useState, useRef, useEffect } from "react";
+
+const Navbar = (props) => {
+  const pathname = usePathname();
+
+  // on hover
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const dropdownRef = useRef(null); // Create a ref for the dropdown element
+
+  const handleDropdownItemClick = (page) => {
+    // Navigate to the selected page here
+    // You can use React Router, window.location.href, or any other navigation method
+    // For example, if you are using React Router, you can use history.push('/your-page')
+  };
+
+  const handleMouseEnter = () => {
+    setShowDropdown(true);
+  };
+
+  const handleMouseLeave = () => {
+    if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+      // Close the dropdown if the click is outside the dropdown
+      setShowDropdown(false);
+    } else if (!dropdownRef.current.contains(event.relatedTarget)) {
+      // Close the dropdown if the mouse leaves the dropdown and doesn't hover over the dropdown button
+      setShowDropdown(false);
+    }
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        // Close the dropdown if the click is outside the dropdown
+        setShowDropdown(false);
+      }
+    };
+
+    window.addEventListener("click", handleClickOutside);
+
+    return () => {
+      window.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+
+  return (
+    <header className="sticky top-0 z-999 flex w-full bg-white drop-shadow-1">
+      <div className="flex flex-grow items-center justify-between px-4 py-2 shadow-2 md:px-6 2xl:px-11">
+        <div className="flex items-center gap-2 sm:gap-4 lg:hidden">
+          {/* <!-- Hamburger Toggle BTN --> */}
+
+          <button
+            aria-controls="sidebar"
+            onClick={(e) => {
+              e.stopPropagation();
+              props.setSidebarOpen(!props.sidebarOpen);
+            }}
+            className="z-99999 block rounded-sm border border-stroke bg-white p-1.5 shadow-sm dark:border-strokedark dark:bg-boxdark lg:hidden"
+          >
+            <span className="relative block h-5.5 w-5.5 cursor-pointer">
+              <span className="du-block absolute right-0 h-full w-full">
+                <span
+                  className={`relative left-0 top-0 my-1 block h-0.5 w-0 rounded-sm bg-black delay-[0] duration-200 ease-in-out dark:bg-white ${
+                    !props.sidebarOpen && "!w-full delay-300"
+                  }`}
+                ></span>
+                <span
+                  className={`relative left-0 top-0 my-1 block h-0.5 w-0 rounded-sm bg-black delay-150 duration-200 ease-in-out dark:bg-white ${
+                    !props.sidebarOpen && "delay-400 !w-full"
+                  }`}
+                ></span>
+                <span
+                  className={`relative left-0 top-0 my-1 block h-0.5 w-0 rounded-sm bg-black delay-200 duration-200 ease-in-out dark:bg-white ${
+                    !props.sidebarOpen && "!w-full delay-500"
+                  }`}
+                ></span>
+              </span>
+              <span className="absolute right-0 h-full w-full rotate-45">
+                <span
+                  className={`absolute left-2.5 top-0 block h-full w-0.5 rounded-sm bg-black delay-300 duration-200 ease-in-out dark:bg-white ${
+                    !props.sidebarOpen && "!h-0 !delay-[0]"
+                  }`}
+                ></span>
+                <span
+                  className={`delay-400 absolute left-0 top-2.5 block h-0.5 w-full rounded-sm bg-black duration-200 ease-in-out dark:bg-white ${
+                    !props.sidebarOpen && "!h-0 !delay-200"
+                  }`}
+                ></span>
+              </span>
+            </span>
+          </button>
+
+          {/* <!-- Hamburger Toggle BTN --> */}
+
+          <Link className="block flex-shrink-0 lg:hidden" href="/">
+            {/* <Image
+              width={40}
+              height={40}
+              src={"/images/logo/logo.png"}
+              alt="Logo"
+            /> */}
+          </Link>
+          <h5 className="text-md font-semibold text-primary pl-1 lg:hidden dark:text-white">
+            CMS
+          </h5>
+        </div>
+
+        {(pathname == "/" || pathname.includes("/")) && (
+          <div className="flex flex-row gap-2 items-center">
+            <Link className=" flex-shrink-0 lg:block hidden" href="/">
+              <Image
+                width={50}
+                height={50}
+                src={"/images/logo/logo.png"}
+                alt="Logo"
+              />
+            </Link>
+            <h5 className="text-lg font-semibold text-primary lg:block hidden dark:text-bodydark1">
+              CMS
+            </h5>
+          </div>
+        )}
+        <div className="hidden lg:block "></div>
+        <div className="flex items-center gap-3 2xsm:gap-7 ">
+          <ul className="flex items-center gap-2 2xsm:gap-4"></ul>
+
+          {/* <!-- User Area --> */}
+          <DropdownUser />
+          {/* <!-- User Area --> */}
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
