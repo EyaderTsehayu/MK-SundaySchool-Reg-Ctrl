af40ed24d8d495853dd7af187b9c7d2020a0b804
Eyader Tsehayu <eyaderts@gmail.com>
2024-01-31 23:09:00 +0300

    modify initial dashboard pages

--- app/(admin)/dashboard/members/add/page.jsx
+++ app/(admin)/dashboard/members/add/page.jsx
@@ -1,5 +1,19 @@
+import { Typography } from "@mui/material";
+import MainCard from "@/components/cards/MainCard";
+import Breadcrumb from "@/components/cards/Breadcrumb";
 const page = () => {
-  return <div>add new member page</div>;
+  return (
+    <div>
+      <Breadcrumb />
+      <MainCard title="Members Registration">
+        <Typography>registration</Typography>
+        <Typography>registration</Typography>
+        <Typography>registration</Typography>
+        <Typography>registration</Typography>
+        <Typography>registration</Typography>
+      </MainCard>
+    </div>
+  );
 };
 
 export default page;
--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,11 @@
-import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+//const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({
+  weight: "400",
+  subsets: ["latin"],
+});
 
 export const metadata = {
   title: "MKSS SRCS ",
@@ -12,7 +16,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={poppins.className}>{children}</body>
     </html>
   );
 }
