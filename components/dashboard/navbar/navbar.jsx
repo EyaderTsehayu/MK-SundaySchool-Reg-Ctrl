c480d5b25d54b6918c3dfc7c78fc3d5b0d00880b
Eyader Tsehayu <eyaderts@gmail.com>
2024-01-31 23:06:49 +0300

    modify dashboard

--- components/dashboard/navbar/navbar.jsx
+++ components/dashboard/navbar/navbar.jsx
@@ -4,6 +4,7 @@ import DropdownUser from "./DropdownUser";
 import Image from "next/image";
 
 import { usePathname } from "next/navigation";
+import MenuIcon from "@mui/icons-material/Menu";
 import { useState, useRef, useEffect } from "react";
 
 const Navbar = (props) => {
@@ -61,71 +62,14 @@ const Navbar = (props) => {
               e.stopPropagation();
               props.setSidebarOpen(!props.sidebarOpen);
             }}
-            className="z-99999 block rounded-sm border border-stroke bg-white p-1.5 shadow-sm dark:border-strokedark dark:bg-boxdark lg:hidden"
+            // className="z-99999 block rounded-sm border border-stroke bg-white p-1.5 shadow-sm dark:border-strokedark dark:bg-boxdark lg:hidden"
           >
-            <span className="relative block h-5.5 w-5.5 cursor-pointer">
-              <span className="du-block absolute right-0 h-full w-full">
-                <span
-                  className={`relative left-0 top-0 my-1 block h-0.5 w-0 rounded-sm bg-black delay-[0] duration-200 ease-in-out dark:bg-white ${
-                    !props.sidebarOpen && "!w-full delay-300"
-                  }`}
-                ></span>
-                <span
-                  className={`relative left-0 top-0 my-1 block h-0.5 w-0 rounded-sm bg-black delay-150 duration-200 ease-in-out dark:bg-white ${
-                    !props.sidebarOpen && "delay-400 !w-full"
-                  }`}
-                ></span>
-                <span
-                  className={`relative left-0 top-0 my-1 block h-0.5 w-0 rounded-sm bg-black delay-200 duration-200 ease-in-out dark:bg-white ${
-                    !props.sidebarOpen && "!w-full delay-500"
-                  }`}
-                ></span>
-              </span>
-              <span className="absolute right-0 h-full w-full rotate-45">
-                <span
-                  className={`absolute left-2.5 top-0 block h-full w-0.5 rounded-sm bg-black delay-300 duration-200 ease-in-out dark:bg-white ${
-                    !props.sidebarOpen && "!h-0 !delay-[0]"
-                  }`}
-                ></span>
-                <span
-                  className={`delay-400 absolute left-0 top-2.5 block h-0.5 w-full rounded-sm bg-black duration-200 ease-in-out dark:bg-white ${
-                    !props.sidebarOpen && "!h-0 !delay-200"
-                  }`}
-                ></span>
-              </span>
-            </span>
+            <MenuIcon sx={{ fontSize: "24px", color: "#5e35b1" }} />
           </button>
 
           {/* <!-- Hamburger Toggle BTN --> */}
-
-          <Link className="block flex-shrink-0 lg:hidden" href="/">
-            {/* <Image
-              width={40}
-              height={40}
-              src={"/images/logo/logo.png"}
-              alt="Logo"
-            /> */}
-          </Link>
-          <h5 className="text-md font-semibold text-primary pl-1 lg:hidden dark:text-white">
-            CMS
-          </h5>
         </div>
 
-        {(pathname == "/" || pathname.includes("/")) && (
-          <div className="flex flex-row gap-2 items-center">
-            <Link className=" flex-shrink-0 lg:block hidden" href="/">
-              <Image
-                width={50}
-                height={50}
-                src={"/images/logo/logo.png"}
-                alt="Logo"
-              />
-            </Link>
-            <h5 className="text-lg font-semibold text-primary lg:block hidden dark:text-bodydark1">
-              CMS
-            </h5>
-          </div>
-        )}
         <div className="hidden lg:block "></div>
         <div className="flex items-center gap-3 2xsm:gap-7 ">
           <ul className="flex items-center gap-2 2xsm:gap-4"></ul>
