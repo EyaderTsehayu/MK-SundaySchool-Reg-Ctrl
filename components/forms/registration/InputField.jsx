2aab4689c0b98a07cb397084d39c86371ba8d06e
Eyader Tsehayu <eyaderts@gmail.com>
2024-02-05 23:46:54 +0300

    add custom reusable mui TextField for registration

---
+++ components/forms/registration/InputField.jsx
@@ -0,0 +1,51 @@
+import {
+  InputLabel,
+  OutlinedInput,
+  FormHelperText,
+  TextField,
+} from "@mui/material";
+import { useField } from "formik";
+
+const InputField = ({ label, ...props }) => {
+  const borderStyle = {
+    fieldset: {
+      borderColor: "rgb(248, 250, 252)",
+      borderRadius: "12px",
+    },
+    input: {
+      backgroundColor: "rgb(248, 250, 252)",
+      borderRadius: "30px",
+    },
+    inputLabel: {
+      fontSize: "15px",
+      fontFamily: "Poppins",
+    },
+  };
+  const borderRad = {
+    "& fieldset": {
+      borderRadius: "8px",
+    },
+  };
+  const [field, meta] = useField(props);
+
+  return (
+    <div>
+      <TextField
+        fullWidth
+        label={label}
+        {...field}
+        {...props}
+        inputProps={{ sx: borderStyle.fieldset }}
+        error={meta.touched && Boolean(meta.error)}
+        sx={borderRad}
+      />
+      {meta.touched && meta.error && (
+        <FormHelperText sx={borderStyle.inputLabel}>
+          {meta.error}
+        </FormHelperText>
+      )}
+    </div>
+  );
+};
+
+export default InputField;
