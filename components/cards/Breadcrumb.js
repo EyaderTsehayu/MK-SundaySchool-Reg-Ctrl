41686ccb3ab97ea534295fff0df001bc9660a168
Eyader Tsehayu <eyaderts@gmail.com>
2024-02-13 23:31:32 +0300

    little modification and dependency installations

--- components/cards/Breadcrumb.js
+++ components/cards/Breadcrumb.js
@@ -3,17 +3,12 @@ import Typography from "@mui/material/Typography";
 import Breadcrumbs from "@mui/material/Breadcrumbs";
 import Link from "@mui/material/Link";
 import HomeIcon from "@mui/icons-material/Home";
-import WhatshotIcon from "@mui/icons-material/Whatshot";
-import GrainIcon from "@mui/icons-material/Grain";
 import { Box } from "@mui/material";
 function handleClick(event) {
   event.preventDefault();
   console.info("You clicked a breadcrumb.");
 }
 const cardContainer = {
-  //   mt: "-4px",
-  //   ml: "-4px",
-  //   mr: "8px",
   display: "flex",
   justifyContent: "space-between",
   alignItems: "center",
@@ -21,29 +16,17 @@ const cardContainer = {
   color: "#000",
   background: "#fff",
   borderRadius: 2,
-  // height: "46px",
   marginBottom: "3px",
   p: "16px",
-
-  //  boxSizing: border-box,
-  // display: flex;
-  // flex-flow: wrap;
-  // margin-top: -8px;
-  // margin-left: -8px;
-  // justify-content: space-between;
-  // align-items: center;
 };
 const titleCont = {
   display: "flex",
 };
 const title = {
-  //margin: 0px;
   fontSize: "1.25rem",
   color: "#0033cc",
-  //color: "rgb(18, 25, 38)",
   fontFamily: "Poppins",
   lineHeight: 1.167,
-  // fontWeight: 550,
 };
 
 const pathCont = {
