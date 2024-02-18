e81667395ca0fd3833824660da28dfabe756d1b6
Eyader Tsehayu <eyaderts@gmail.com>
2024-02-18 23:26:37 +0300

    add parent profile edit and view form

---
+++ components/forms/details/ParentInformation.jsx
@@ -0,0 +1,68 @@
+import SubCard from "@/components/cards/SubCard";
+import { TextField, Grid } from "@mui/material";
+import ProfileHeader from "./ProfileHeader";
+import { useFormik } from "formik";
+
+const ParentInformation = ({ onSubmit, initialValues }) => {
+  const formik = useFormik({
+    initialValues: initialValues,
+    onSubmit: (values) => {
+      console.log("Form values submitted:", values);
+      if (onSubmit) {
+        onSubmit(values);
+      }
+    },
+  });
+
+  return (
+    <div className="flex flex-col gap-2">
+      <ProfileHeader />
+      <SubCard>
+        {" "}
+        <form onSubmit={formik.handleSubmit}>
+          <Grid container spacing={2}>
+            <Grid item xs={12} md={6}>
+              <TextField
+                id="guardianName"
+                label="Parent/Guardian Name"
+                variant="outlined"
+                fullWidth
+                margin="normal"
+                {...formik.getFieldProps("guardianName")}
+                error={
+                  formik.touched.guardianName &&
+                  Boolean(formik.errors.guardianName)
+                }
+                helperText={
+                  formik.touched.guardianName && formik.errors.guardianName
+                }
+              />
+            </Grid>{" "}
+            <Grid item xs={12} md={6}>
+              <TextField
+                id="parentPhoneNumber"
+                label=" Parent Phone Number"
+                variant="outlined"
+                fullWidth
+                margin="normal"
+                {...formik.getFieldProps("parentPhoneNumber")}
+                error={
+                  formik.touched.parentPhoneNumber &&
+                  Boolean(formik.errors.parentPhoneNumber)
+                }
+                helperText={
+                  formik.touched.parentPhoneNumber &&
+                  formik.errors.parentPhoneNumber
+                }
+              />
+            </Grid>
+          </Grid>
+
+          <button type="submit">Save</button>
+        </form>
+      </SubCard>
+    </div>
+  );
+};
+
+export default ParentInformation;
