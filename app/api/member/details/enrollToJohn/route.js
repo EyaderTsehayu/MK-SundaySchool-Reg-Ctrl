3f784762f2e564eb6d3875df943b54f391766b5b
Eyader Tsehayu <eyaderts@gmail.com>
2024-03-08 23:58:06 +0300

    add an api for enrolling to next step

---
+++ app/api/member/details/enrollToJohn/route.js
@@ -0,0 +1,24 @@
+import { connectToDB } from "@/utils/database";
+import Member from "@/models/member";
+
+export const PATCH = async (request) => {
+  try {
+    await connectToDB();
+    const { rowSelectionModel } = await request.json();
+    const result = await Member.updateMany(
+      { _id: { $in: rowSelectionModel } },
+      { $set: { division: "John The Baptist" } }
+    );
+    // Respond with a success message or any relevant data
+    if (!result) {
+      return new Response("Unable to enroll", { status: 404 });
+    }
+
+    return new Response("Enrolled succesfully", { status: 200 });
+  } catch (error) {
+    console.error("Error updating data:", error);
+    return new Response("Failed to fetch member details", {
+      status: 500,
+    });
+  }
+};
