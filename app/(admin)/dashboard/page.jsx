cbf24a560b599f54e53b1543c078565b6a446bbc
Eyader Tsehayu <eyaderts@gmail.com>
2024-03-17 23:16:51 +0300

    fetch and pass the data to charts and featured

--- app/(admin)/dashboard/page.jsx
+++ app/(admin)/dashboard/page.jsx
@@ -13,6 +13,26 @@ import { useEffect, useState } from "react";
 
 const Dashboard = () => {
   const [members, setMembers] = useState(null);
+  const [data, setData] = useState(null);
+
+  // const [department, setDepartment] = useState([]);
+  // const [workcondition, setWorkcondition] = useState([]);
+
+  useEffect(() => {
+    const fetchDepartments = async () => {
+      try {
+        const response = await fetch("/api/member");
+        const data = await response.json();
+
+        if (response.ok) {
+          setData(data);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchDepartments();
+  }, []);
 
   useEffect(() => {
     const fetchMembers = async () => {
@@ -28,58 +48,62 @@ const Dashboard = () => {
     fetchMembers();
   }, []);
 
-  console.log("members in dashboard main page", members);
-
   if (members === null) {
     // Loading state or you can show a loading spinner
-    return <div>Loading...</div>;
+    return (
+      <div className="flex h-screen items-center justify-center bg-white/80">
+        <div className="h-16 w-16 animate-spin rounded-full border-4 border-solid border-primary border-t-transparent"></div>
+      </div>
+    );
   }
 
   return (
     <>
-      <Box>
+      {members && data && (
         <Box>
-          <Grid container spacing={3}>
-            <Grid item xs={12} sm={6} md={6} lg={4}>
-              <Students
-                count={members.totalMembers}
-                type="student"
-                className="card"
-              />
-            </Grid>
-            <Grid item xs={12} sm={6} md={6} lg={4}>
-              <Teachers type="teacher" className="card" />
-            </Grid>
+          <Box>
+            <Grid container spacing={3}>
+              <Grid item xs={12} sm={6} md={6} lg={4}>
+                <Students
+                  count={members.totalMembers}
+                  type="student"
+                  className="card"
+                />
+              </Grid>
+              <Grid item xs={12} sm={6} md={6} lg={4}>
+                <Teachers type="teacher" className="card" />
+              </Grid>
 
-            <Grid item xs={12} sm={12} md={12} lg={4}>
-              <Box>
-                <Grid container spacing={3}>
-                  <Grid item xs={12} sm={6} md={6} lg={12}>
-                    <MaleStudents
-                      count={members.maleMembers}
-                      type="module"
-                      className="card"
-                    />
+              <Grid item xs={12} sm={12} md={12} lg={4}>
+                <Box>
+                  <Grid container spacing={3}>
+                    <Grid item xs={12} sm={6} md={6} lg={12}>
+                      <MaleStudents
+                        count={members.maleMembers}
+                        type="module"
+                        className="card"
+                      />
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={6} lg={12}>
+                      <FemaleStudents
+                        count={members.femaleMembers}
+                        type="module"
+                        className="card"
+                      />
+                    </Grid>
                   </Grid>
-                  <Grid item xs={12} sm={6} md={6} lg={12}>
-                    <FemaleStudents
-                      count={members.femaleMembers}
-                      type="module"
-                      className="card"
-                    />
-                  </Grid>
-                </Grid>
-              </Box>
+                </Box>
+              </Grid>
+              <Grid item xs={12} sm={12} md={12} lg={8}>
+                <Chart data={data} />
+              </Grid>
+              <Grid item xs={12} sm={12} md={12} lg={4}>
+                <Featured data={data} />
+              </Grid>
             </Grid>
-            <Grid item xs={12} sm={12} md={12} lg={8}>
-              <Chart />
-            </Grid>
-            <Grid item xs={12} sm={12} md={12} lg={4}>
-              <Featured />
-            </Grid>
-          </Grid>
+          </Box>
         </Box>
-      </Box>
+      )}
     </>
   );
 };
